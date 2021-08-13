894531da05407b92075f06f46bc524c2e298729e
José Victor Santos Vieira <criolo444@gmail.com>
2021-08-13 05:50:06 -0300

    Adicionando bloco try catch

--- java-excecoes/pilha-de-execucao/src/pilha/Fluxo.java
+++ java-excecoes/pilha-de-execucao/src/pilha/Fluxo.java
@@ -1,26 +1,36 @@
 package pilha;
 
 public class Fluxo {
-	
+
 	public static void main(String[] args) {
-		
-        System.out.println("Inicio do main");
-        metodo1();
-        System.out.println("Fim do main");
-    }
 
-    private static void metodo1() {
-        System.out.println("Inicio do metodo1");
-        metodo2();
-        System.out.println("Fim do metodo1");
-    }
+		System.out.println("Inicio do main");
+		metodo1();
+		System.out.println("Fim do main");
 
-    private static void metodo2() {
-        System.out.println("Inicio do metodo2");
-        for(int i = 1; i <= 5; i++) {
-            System.out.println(i);
-        }
-        System.out.println("Fim do metodo2");
-    }
+	}
+
+	private static void metodo1() {
+		System.out.println("Inicio do metodo1");
+		metodo2();
+		System.out.println("Fim do metodo1");
+	}
+
+	private static void metodo2() {
+		System.out.println("Inicio do metodo2");
+
+		try {
+
+			for (int i = 1; i <= 5; i++) {
+				System.out.print(i + " ");
+			}
+
+			System.out.println("\nFim do metodo2");
+			int excecao = 2 / 0;
+
+		} catch (Exception e) {
+
+			System.out.println("--> ArithmeticException <--");
+		}
+	}
 }
-
